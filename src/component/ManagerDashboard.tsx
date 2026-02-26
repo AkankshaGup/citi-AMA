0f0e9706a69100ca663d35c3c19a74c143ba5a25
Akanksha Gupta <akagupta@altimetrik.com>
2026-02-26 11:49:40 +0530

    removed mockdata

--- src/component/ManagerDashboard.tsx
+++ src/component/ManagerDashboard.tsx
@@ -7,7 +7,7 @@ import { api } from "../api/axiosInstance";
 import React, { useEffect, useState } from "react";
 import { addMonths, subMonths, startOfMonth, format } from "date-fns";
 import { auth } from "../auth/auth";
-import { Typography, Button } from "@mui/material";
+import { Typography, Button, Alert } from "@mui/material";
 import { mockTeamData } from "../metadata/metadata";
 
 interface TeamResource {
@@ -20,17 +20,18 @@ export default function ManagerDashboard() {
 	const currentMonthStart = React.useMemo(() => startOfMonth(new Date()), []);
 	const [month, setMonth] = React.useState<Date>(currentMonthStart);
 	const [teamData, setTeamData] = useState<TeamResource[]>([]);
+	const [errorMsg, setErrorMsg] = useState<string | null>(null);
 	const [selectedSow, setSelectedSow] = useState<TeamResource | null>(null);
 	const user = auth.getUser();
 
 	const fetchTeam = async () => {
+		setErrorMsg(null);
 		try {
 			const res = await api.get(`public/sows/manager/${user.userId}`);
 			setTeamData(res.data);
 			setSelectedSow(res.data[0] ?? null);
-		} catch {
-			setTeamData(mockTeamData);
-			setSelectedSow(mockTeamData[0] ?? null);
+		} catch (err) {
+			setErrorMsg(err instanceof Error ? err.message : String(err));
 		}
 	};
 
@@ -38,15 +39,15 @@ export default function ManagerDashboard() {
 		fetchTeam();
 	}, []);
 
-    const handlePrev = () => {
-        setMonth((m) => subMonths(m, 1));
-    };
+	const handlePrev = () => {
+		setMonth((m) => subMonths(m, 1));
+	};
 
-    const handleNext = () => {
+	const handleNext = () => {
 
-        setMonth((m) => addMonths(m, 1));
-    };
-    const monthTitle = format(month, "MMMM yyyy");
+		setMonth((m) => addMonths(m, 1));
+	};
+	const monthTitle = format(month, "MMMM yyyy");
 	return (
 		<>
 			<Box display="flex" justifyContent="space-between" marginBottom={4} marginTop={2}>
@@ -86,10 +87,14 @@ export default function ManagerDashboard() {
 					Export
 				</Button>
 			</Box>
+			{errorMsg && (
+				<Alert severity="error" onClose={() => setErrorMsg(null)} sx={{ mb: 2 }}>
+					{errorMsg}
+				</Alert>
+			)}
+			{selectedSow?.sowId && (<><TimesheetHeader title='' monthTitle={monthTitle} onPrev={handlePrev} onNext={handleNext} />
 
-        <TimesheetHeader title='' monthTitle={monthTitle} onPrev={handlePrev} onNext={handleNext} />
-			{selectedSow?.sowId && (
-				<ResourseTable sowId={selectedSow.sowId} month={month} />
+				<ResourseTable sowId={selectedSow.sowId} month={month} /></>
 			)}
 		</>
 	);
