5a46c2ed707dd0ac41e8c612dde14415d8c9fcb4
Akanksha Gupta <akagupta@altimetrik.com>
2026-03-02 15:52:54 +0530

    env file integrated

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,9 +1,15 @@
 import { useMutation } from "@tanstack/react-query";
 import { loginApi } from "../api/authApi.ts";
 import type { LoginPayload } from "../api/authApi.ts";
+import { loginRes } from "../metadata/metadata.ts";
 
 export const useLogin = () => {
   return useMutation({
-    mutationFn: (payload: LoginPayload) => loginApi(payload),
+    mutationFn: async (payload: LoginPayload) => {
+      if (import.meta.env.VITE_USE_MOCK === "true") {
+        return loginRes;
+      }
+      return loginApi(payload);
+    },
   });
 };
